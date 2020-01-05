d7f57f46819234b8290002af150b931df63cc688
SergeRodriguez <serge.rodriguez@mail.mcgill.ca>
2020-01-05 18:44:29 -0500

    update database schema

---
+++ db/04_requests.sql
@@ -0,0 +1,15 @@
+-- Drop and recreate requests table (Example)
+
+DROP TABLE IF EXISTS requests CASCADE;
+CREATE TABLE requests (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id),
+  business_id INTEGER REFERENCES businesses(id),
+  business_category_id INTEGER REFERENCES business_categories(id) NOT NULL,
+  service_category_id INTEGER REFERENCES service_categories(id) NOT NULL,
+  status_id INTEGER REFERENCES statuses(id) NOT NULL,
+  created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+  is_prepaid BOOLEAN NOT NULL DEFAULT FALSE,
+  start_time DATETIME NOT NULL,
+  end_time DATETIME NOT NULL
+);
