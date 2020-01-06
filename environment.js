c71c310041eecd8cd3f26c54a165758a7899d980
Jeff Gebert <jeffgebert@hotmail.com>
2020-01-06 13:41:46 -0500

    comit before merging origin master

--- environment.js
+++ environment.js
@@ -1,11 +1,9 @@
 const path = require("path");
-console.log("path", path)
 
 const ENV = process.env.NODE_ENV || "development";
 const PATH = path.resolve(__dirname, "../.env." + ENV);
-console.log("path2", PATH)
 
 require("dotenv").config({ path: PATH });
-console.log("ENV", ENV);
+
 module.exports = ENV;
 
