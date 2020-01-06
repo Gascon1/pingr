e2052500b8a5587effee94211ecd23cb472d5153
Gascon1 <mikelgascon@outlook.com>
2020-01-06 10:50:15 -0500

    add button to navigate to search form, change background color for dev purposes

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,14 @@ import LandingPage from "./LandingPage";
 import LoginPage from "./LoginPage";
 import RegisterPage from "./RegisterPage";
 import HomePage from "./HomePage";
+import SearchForm from "./SearchForm";
 import useVisualMode from "./hooks/useVisualMode";
 
 const LANDINGPAGE = "LANDINGPAGE";
 const LOGIN = "LOGIN";
 const REGISTER = "REGISTER";
 const HOMEPAGE = "HOMEPAGE";
+const SEARCHFORM = "SEARCHFORM";
 
 function App() {
   const { mode, transition, back } = useVisualMode(HOMEPAGE);
@@ -31,7 +33,11 @@ function App() {
       )}
       {mode === LOGIN && <LoginPage back={back} />}
       {mode === REGISTER && <RegisterPage back={back} />}
-      {mode === HOMEPAGE && <HomePage />}
+      {mode === HOMEPAGE && (
+        <HomePage transition={transition} searchForm={SEARCHFORM} />
+      )}
+
+      {mode === SEARCHFORM && <SearchForm back={back} />}
     </main>
   );
 }
---
+++ client/src/SearchForm.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SearchForm(props) {
+  return (
+    <div>
+      <div>this is a test</div>
+    </div>
+  );
+}
