91d07adedbf9c78920da359df8503801df0c96e6
Jeff Gebert <jeffgebert@hotmail.com>
2020-01-06 11:26:29 -0500

    created application.js file

--- index.js
+++ index.js
@@ -2,3 +2,16 @@ const PORT = process.env.PORT || 8001;
 const ENV = require("./environment");
 
 
+const app = require("./application")(ENV, { updateAppointment });
+const server = require("http").Server(app);
+
+const WebSocket = require("ws");
+const wss = new WebSocket.Server({ server });
+
+
+wss.on("connection", socket => {
+    socket.onmessage = event => {
+      console.log(`connected`);
+    };
+  });
+  
