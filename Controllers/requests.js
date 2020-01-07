5edb0ad2c1efc462029e2eefd5bae0d5a37408d5
Jeff Gebert <jeffgebert@hotmail.com>
2020-01-06 19:04:59 -0500

    created requests controller and db.js file

---
+++ Controllers/requests.js
@@ -0,0 +1,16 @@
+const db = require('./db.js')
+
+module.exports = (req, res) => {
+	// console.log('req.query', req.query)
+	let query = `SELECT * FROM requests'
+`
+
+
+	db.query(query, (err, result) => {
+		if (err) {
+			res.send(err)
+		} else {
+			res.send(result.rows)
+		}
+	})
+}
