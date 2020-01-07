c149215424c4d3d8503982869453ca55dde55ed3
SergeRodriguez <serge.rodriguez@mail.mcgill.ca>
2020-01-07 16:45:15 +0000

    updating seeds

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,5 +1,5 @@
 -- Users table seeds here (Example)
 INSERT INTO users (first_name, last_name, phone, email, password)
-VALUES ('Mikel', 'Shotcaller','5141234567', 'mikelangelo@gmail.com', '12345');
-VALUES ('Jeff', 'Jeffster','5145678901', 'jeffstein@gmail.com', '12345');
+VALUES ('Mikel', 'Shotcaller','5141234567', 'mikelangelo@gmail.com', '12345'),
+VALUES ('Jeff', 'Jeffster','5145678901', 'jeffstein@gmail.com', '12345'),
 VALUES ('Serge', 'Testmaster','5147890123', 'serge@gmail.com', '12345');
--- db/seeds/02_businesses.sql
+++ db/seeds/02_businesses.sql
@@ -1,6 +1,6 @@
 -- Businesses table seeds here (Example)
 INSERT INTO businesses (name, email, phone, address , business_category_id)
-VALUES ('Vitality Massage', 'vitality@massage.com','5143234123', '3210 arcand', 1);
-VALUES ('Real Good Massage', 'realgood@massage.com','5142302930', '8990 Sherbrooke St E, Montreal, Quebec H1L 1C9', 1);
-VALUES ("Mademois'Elles", 'info@studiomademoiselle.ca','5143234123', '273 Rue Beaubien E, Montréal, QC H2S 1R7', 2);
+VALUES ('Vitality Massage', 'vitality@massage.com','5143234123', '3210 arcand', 1),
+VALUES ('Real Good Massage', 'realgood@massage.com','5142302930', '8990 Sherbrooke St E, Montreal, Quebec H1L 1C9', 1),
+VALUES ("Mademois'Elles", 'info@studiomademoiselle.ca','5143234123', '273 Rue Beaubien E, Montréal, QC H2S 1R7', 2),
 VALUES ("Kinatex Sports Physio Jarry-Petite Italie", 'kinatex@gmail.com', '5142720101', '25 Avenue Mozart E #301', 3);
