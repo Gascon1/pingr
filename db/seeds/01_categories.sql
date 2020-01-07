da25501d1886aa85b80309f39051e9969c27cc20
Mikel Gascon <mikelgascon@outlook.com>
2020-01-07 16:49:40 +0000

    change seeds end character from semi colon to commas

--- db/seeds/01_categories.sql
+++ db/seeds/01_categories.sql
@@ -1,5 +1,5 @@
 -- Business categories table seeds here (Example)
 INSERT INTO categories(name)
-VALUES ('massotherapy');
-VALUES ('beauty salon');
+VALUES ('massotherapy'),
+VALUES ('beauty salon'),
 VALUES ('physiotherapy');
--- db/seeds/04_statuses.sql
+++ db/seeds/04_statuses.sql
@@ -1,8 +1,8 @@
 -- Statuses table seeds here (Example)
 INSERT INTO statuses (name)
-VALUES ('queued');
-VALUES ('matched');
-VALUES ('confirmed');
-VALUES ('completed');
-VALUES ('expired');
+VALUES ('queued'),
+VALUES ('matched'),
+VALUES ('confirmed'),
+VALUES ('completed'),
+VALUES ('expired'),
 VALUES ('canceled');
