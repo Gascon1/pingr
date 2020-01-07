7a429705f263654fbeada1e65e7f302a70424911
SergeRodriguez <serge.rodriguez@mail.mcgill.ca>
2020-01-07 17:01:50 +0000

    fix seeds for the routes

--- db/seeds/01_categories.sql
+++ db/seeds/01_categories.sql
@@ -1,5 +1,5 @@
 -- Business categories table seeds here (Example)
 INSERT INTO categories(name)
 VALUES ('massotherapy'),
-VALUES ('beauty salon'),
-VALUES ('physiotherapy');
+ ('beauty salon'),
+ ('physiotherapy');
--- db/seeds/05_services.sql
+++ db/seeds/05_services.sql
@@ -1,13 +1,13 @@
 -- Services table seeds here (Example)
 INSERT INTO services (name, business_id, category_id, description, price, duration)
 VALUES ('swedish massage', 1, 1, 'a swedish massage is a relaxing, full-body massage.', 90.00, 60.00),
-VALUES ('swedish massage', 1, 1, 'a swedish massage is a relaxing, full-body massage.', 50.00, 30.00),
+       ('swedish massage', 1, 1, 'a swedish massage is a relaxing, full-body massage.', 50.00, 30.00),
 
-VALUES ('swedish massage', 2, 1, 'a swedish massage is a relaxing, full-body massage.', 70.00, 60.00),
-VALUES ('swedish massage', 2, 1, 'a swedish massage is a relaxing, full-body massage.', 70.00, 60.00),
+       ('swedish massage', 2, 1, 'a swedish massage is a relaxing, full-body massage.', 70.00, 60.00),
+       ('swedish massage', 2, 1, 'a swedish massage is a relaxing, full-body massage.', 70.00, 60.00),
 
-VALUES ('Épilation', 3, 2, 'bikini complet', 34.99, 30.00),
-VALUES ('Épilation', 3, 2, 'Cuisses', 25.99, 30.00),
-VALUES ('Épilation', 3, 2, 'Jambes complètes et bikini complet', 73.00, 90.00),
+       ('Épilation', 3, 2, 'bikini complet', 34.99, 30.00),
+       ('Épilation', 3, 2, 'Cuisses', 25.99, 30.00),
+       ('Épilation', 3, 2, 'Jambes complètes et bikini complet', 73.00, 90.00),
 
-VALUES ('Physical therapy', 4, 3, 'Sports therapy', 55.00, 60.00);
+       ('Physical therapy', 4, 3, 'Sports therapy', 55.00, 60.00);
