3f7bb99bd4ce7e51af1ad1c7d4dd72a632872a18
Jeff Gebert <jeffgebert@hotmail.com>
2020-01-07 17:55:51 -0500

    deleted users route

---
+++ routes/get_businesses.js
@@ -0,0 +1,13 @@
+const requestsController = require("../Controllers/get_businesses.js")
+const router = require("express").Router();
+
+module.exports = (request, response) => {
+  router.post('/', function (request, response) {
+      requestsController(request, response)
+      .then((data) => {
+          console.log(data)
+          response.json(data);
+        });
+  })
+  return router;
+};
