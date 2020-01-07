985bd9477fedb1aeaae93bae5f5a02973e1ab23f
Jeff Gebert <jeffgebert@hotmail.com>
2020-01-07 18:08:43 -0500

    minor change

--- Controllers/post_create_users.js
+++ Controllers/post_create_users.js
@@ -1,7 +1,7 @@
 const db = require('./db.js')
 
 module.exports = (req, res) => {
-	 console.log('req.query', req.query)
+	 console.log('req.query', req.body)
 	let query = `INSERT INTO users (first_name, last_name, phone, email, password)
     VALUES ('Lucas', 'Spicemaster','5141994567', 'lucas@gmail.com', '12345');`
 	db.query(query, (err, result) => {
