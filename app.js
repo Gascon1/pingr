2223141b190bfa9121731285ff6229cae9dffe5a
Jeff Gebert <jeffgebert@hotmail.com>
2020-01-07 17:08:05 -0500

    Deleted irrelevant routes and controllers and added get routes and controllers for active and expired requests.  Also added put routes and controllers for creating a user as well as a request

--- app.js
+++ app.js
@@ -7,13 +7,10 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var ActiveRequests = require('./routes/get_active_requests_by_user');
 var ExpiredRequests = require('./routes/get_expired_requests_by_user');
-var businesses = require('./routes/businesses');
-var categories = require('./routes/categories');
-var users = require('./routes/users');
-var statuses = require('./routes/statuses');
-var services = require('./routes/services');
 
 
+var CreateRequests = require('./routes/post_create_requests');
+var CreateUsers = require('./routes/post_create_users');
 
 
 
@@ -32,19 +29,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/active_requests', ActiveRequests);
 app.use('/expired_requests', ExpiredRequests);
-app.use('/businesses', businesses);
-app.use('/categories', categories);
-app.use('/users', users);
-app.use('/statuses', statuses);
-app.use('/services', services);
+
+app.use('/create_requests', CreateRequests);
+app.use('/create_users', CreateUsers);
+
+
 
 app.get('/api/active_requests', require('./Controllers/get_active_requests_by_user.js'))
 app.get('/api/expired_requests', require('./Controllers/get_expired_requests_by_user.js'))
-app.get('/api/businesses', require('./Controllers/businesses.js'))
-app.get('/api/categories', require('./Controllers/categories.js'))
-app.get('/api/users', require('./Controllers/users.js'))
-app.get('/api/statuses', require('./Controllers/statuses.js'))
-app.get('/api/services', require('./Controllers/services.js'))
+app.post('/api/create_requests', require('./Controllers/post_create_requests.js'))
+app.post('/api/create_users', require('./Controllers/post_create_users.js'))
 
 
 
