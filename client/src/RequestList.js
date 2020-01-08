858d4012f4984b88d9bb19b596c5f5cd75105c8a
SergeRodriguez <serge.rodriguez@mail.mcgill.ca>
2020-01-08 10:07:57 -0500

    added a return to the requestlist component

---
+++ client/src/RequestList.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function (props) {
+  
+  const [state, setState] = useState([])
+
+  useEffect(() => {
+   axios.get(`http://localhost:8001/api/requests`).then((response) => {
+    return setState(response.data);
+  });
+}, [])
+
+const list = state.map((request) => {
+  return <ActiveRequestsItem
+   key={day.id}
+   name={day.name}
+   spots={day.spots}
+   selected={day.name === props.day}
+   setDay={props.setDay} />
+ })
+
+ return (
+  <ul>
+    {list}
+  </ul>
+)
+ 
+
+}
+  
