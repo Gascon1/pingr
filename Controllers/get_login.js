761eb2e26998e60d95f4879e93585b7cec4e4e8b
Jeff Gebert <jeffgebert@hotmail.com>
2020-01-08 16:35:54 -0500

    added hashed password to the create user route

--- Controllers/get_login.js
+++ Controllers/get_login.js
@@ -9,19 +9,23 @@ module.exports = (req, res) => {
 	db.query(`SELECT email, password from users where email = '${req.body.email}'`, (err, result) => {
 		if (err) {
 			res.send('Sorry, email not found')
-		} else {
-			if (result.rows[0].password === req.body.password) {
-				
-				let token = jwt.sign({user:result.email}, process.env.SECRET)
-				res.status(200).json({
-					message: 'You are logged in',
-					token: token
-				})
+		} else {			
+			bcrypt.compare(req.body.password,result.rows[0].password, (err, match) => {
 
-			}else{
-				res.send('Sorry, invalid password')
-			}
+				if (match) {
+					let token = jwt.sign({user:result.rows[0].email}, process.env.SECRET)
+					res.status(200).json({
+						message: 'You are logged in',
+						token: token
+					})
+				}else{
+					res.send('Sorry, invalid password')
+				}
+
+			})
+
 		}
 	})
 
 }
+
--- Controllers/post_users.js
+++ Controllers/post_users.js
@@ -1,5 +1,5 @@
 const db = require('./db.js')
-//const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 
 
 module.exports = (req, res) => {
@@ -7,7 +7,7 @@ module.exports = (req, res) => {
 	let query = {
 		text:`INSERT INTO users (first_name, last_name, phone, email, password)
 	VALUES ($1, $2, $3, $4, $5);`,
-	values: [req.body.first_name, req.body.last_name, req.body.phone, req.body.email, req.body.password]
+	values: [req.body.first_name, req.body.last_name, req.body.phone, req.body.email, bcrypt.hashSync(req.body.password, 10)]
 }
 	db.query(query, (err, result) => {
 		if (err) {
