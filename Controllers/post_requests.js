6013e6d26845b9f0cbae7440a26cbe894e6adb30
Jeff Gebert <jeffgebert@hotmail.com>
2020-01-07 20:38:11 -0500

    added untracked files

---
+++ Controllers/post_requests.js
@@ -0,0 +1,17 @@
+const db = require('./db.js')
+
+module.exports = (req, res) => {
+	let query = `INSERT INTO requests (user_id, category_id, service_id, status_id, created_at, start_time, end_time, is_prepaid)
+    VALUES (2,3,4,1, current_date, '2020-01-10 18:00:00', '2020-01-10 19:00:00', false);`
+
+	db.query(query, (err, result) => {
+		if (err) {
+            res.send(err)
+            console.log("error", err)
+		} else {
+            console.log("result", result)
+			res.send(result.rows)
+		}
+    })
+    
+}
