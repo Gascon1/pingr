064340167aefda036d33466d6f3c1624e4c8c61f
Gascon1 <mikelgascon@outlook.com>
2020-01-12 16:12:43 -0500

    business owners can create services, users can choose those services when creating a request, and business owners can confirm those requests

--- db/seeds/02_businesses.sql
+++ db/seeds/02_businesses.sql
@@ -1,7 +1,4 @@
 -- Businesses table seeds here (Example)
 INSERT INTO businesses (name, email, phone, address , category_id)
 VALUES 
-('No business', 'No email','No phone', 'No address', null),
-('Real Good Massage', 'realgood@massage.com','514-230-2930', '8990 Sherbrooke St E, Montreal, Quebec H1L 1C9', 1),
-('MademoisElles', 'info@studiomademoiselle.ca','514-323-4123', '273 Rue Beaubien E, Montréal, QC H2S 1R7', 2),
-('Kinatex Sports Physio Jarry-Petite Italie', 'kinatex@gmail.com', '514-272-0101', '25 Avenue Mozart E #301', 3);
+('No business', 'No email','No phone', 'No address', null);
